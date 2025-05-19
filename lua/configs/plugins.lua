90f799f51d16d1b7c20ecd94cdd494f453e72791
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2025-05-19 12:20:33 +0300

    поправил комментарии

--- lua/configs/plugins.lua
+++ lua/configs/plugins.lua
@@ -121,6 +121,7 @@ return {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
