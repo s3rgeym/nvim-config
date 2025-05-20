29168d73524eb74156ce5818d8cb8cf2a35e89fc
[object Object] <tz4678@gmail.com>
2025-05-20 07:24:42 +0300

    коммент

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -332,7 +332,10 @@ return {
     priority = 1000,
     lazy = false, -- Загружаем сразу
     opts = {
-      -- Включать прозрачность для темы не нужно, если установить colors.transparent_background_colors = true в настройках alacritty
+      -- Geovide как-то неправильно работает с этой прозрачностью (через
+      -- bg=None).
+      -- Включать прозрачность для темы не нужно, если установить
+      -- colors.transparent_background_colors = true в настройках alacritty
       -- transparent = true,
       -- styles = {
       --   sidebars = "transparent",
