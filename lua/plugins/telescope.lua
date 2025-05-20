a586e375a830c68c9ec8d0617092578daa1411ce
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2025-05-20 14:38:36 +0300

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,8 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = true,
   keys = {
-    { "<leader>/", "<cmd>Telescope find_files<cr>", desc = "Find file" },
-    { "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Find using grep" },
+    { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Find using grep" },
+    { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find file" },
     { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Find buffer" },
   },
 }
