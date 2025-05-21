677d2ee7bcf67925e984e4938d9b82e99f13706d
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2025-05-21 08:07:15 +0300

    форматирование, увеличил размер всплывающего меню

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,12 @@ return {
   config = true,
   keys = {
     -- Поиск
-    { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Find using grep" },
-    { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find file" },
-    { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Find buffer" },
+    { "<leader>/",  "<cmd>Telescope live_grep<cr>",           desc = "Find using grep" },
+    { "<leader>f",  "<cmd>Telescope find_files<cr>",          desc = "Find file" },
+    { "<leader>b",  "<cmd>Telescope buffers<cr>",             desc = "Find buffer" },
     -- Работа с LSP
-    { "<leader>qf", "<cmd>Telescope quickfix<cr>", desc = "QuickFix" },
-    { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
-    { "gi", "<cmd>Telescope lsp_implementations<cr>", desc = "Implementations" },
+    { "<leader>qf", "<cmd>Telescope quickfix<cr>",            desc = "QuickFix" },
+    { "gr",         "<cmd>Telescope lsp_references<cr>",      desc = "References" },
+    { "gi",         "<cmd>Telescope lsp_implementations<cr>", desc = "Implementations" },
   },
 }
