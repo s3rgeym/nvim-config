0b782b215ae544d275d620a872361106bcbdc548
[object Object] <tz4678@gmail.com>
2025-05-22 09:01:53 +0300

    .

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,18 @@ return {
   config = true,
   keys = {
     -- Поиск
-    { "<leader>/",  "<cmd>Telescope live_grep<cr>",           desc = "Find using grep" },
-    { "<leader>f",  "<cmd>Telescope find_files<cr>",          desc = "Find file" },
-    { "<leader>b",  "<cmd>Telescope buffers<cr>",             desc = "Find buffer" },
+    { "<leader>/",  "<cmd>Telescope live_grep<cr>",             desc = "Find using grep" },
+    { "<leader>f",  "<cmd>Telescope find_files<cr>",            desc = "Find file" },
+    { "<leader>b",  "<cmd>Telescope buffers<cr>",               desc = "Find buffer" },
+
     -- Работа с LSP
-    { "<leader>qf", "<cmd>Telescope quickfix<cr>",            desc = "QuickFix" },
-    { "gr",         "<cmd>Telescope lsp_references<cr>",      desc = "References" },
-    { "gi",         "<cmd>Telescope lsp_implementations<cr>", desc = "Implementations" },
+    { "<leader>qf", "<cmd>Telescope quickfix<cr>",              desc = "QuickFix" },
+    { "gr",         "<cmd>Telescope lsp_references<cr>",        desc = "References" },
+    { "gi",         "<cmd>Telescope lsp_implementations<cr>",   desc = "Implementations" },
+    { "gd",         "<cmd>Telescope lsp_definitions<cr>",       desc = "Definitions" },
+    { "<leader>d",  "<cmd>Telescope diagnostics<cr>",           desc = "Diagnostics (workspace)" },
+    { "<leader>ds", "<cmd>Telescope lsp_document_symbols<cr>",  desc = "Document Symbols" },
+    { "<leader>ws", "<cmd>Telescope lsp_workspace_symbols<cr>", desc = "Workspace Symbols" },
+    { "<leader>ll", "<cmd>Telescope loclist<cr>",               desc = "Location List" },
   },
 }
