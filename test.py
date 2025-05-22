f8dfe06a7b542eb9bcdbd925650d8931b273daa8
[object Object] <tz4678@gmail.com>
2025-05-22 06:41:37 +0300

    f9 - toggle breakpoint

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 
 def main():
     r = requests.post("https://ipinfo.io")
-    # Поставь тут брейкпоинт с помощью Alt-b и запусти дебаггер, нажав F5
+    # Поставь тут брейкпоинт с помощью F9 и запусти дебаггер, нажав F5
     data = r.json()
     print("Your IP:", data["ip"])
 
