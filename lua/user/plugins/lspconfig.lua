4dfc059a522e0e36a2c164a2075d9e278d0545e5
[object Object] <tz4678@gmail.com>
2025-06-03 15:39:20 +0300

    .

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -1,8 +1,6 @@
 local map = require("user.utils").map
 
-local M = {}
-
-function M.on_attach(_, bufnr)
+local function on_attach(_, bufnr)
   local function nmap(keys, func, desc)
     map('n', keys, func, "LSP: " .. desc, { buffer = bufnr })
   end
@@ -34,4 +32,41 @@ function M.on_attach(_, bufnr)
   nmap("<leader>fq", "<cmd>FzfLua grep_quickfix<CR>", "Quickfix")
 end
 
-return M
+-- :help lspconfig-all
+-- Список серверов: https://github.com/neovim/nvim-lspconfig/tree/master/lsp
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    local capabilities = require('cmp_nvim_lsp').default_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+    )
+
+    -- Автоматическая установка и настройка языковых серверов
+    -- Вынес в отдельный файл
+    -- require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "pyright", "ruff", "gopls" },
+      automatic_installation = true,
+    })
+
+    vim.lsp.config("*", {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+
+    -- Настройка уже установленных серверов
+    local servers = {
+      -- Сюда вписываем названия серверов, которые уже установлены в системе
+      -- 'rust-analyzer'
+    }
+
+    for _, lsp in ipairs(servers) do
+      vim.lsp.enable(lsp)
+    end
+  end
+}
