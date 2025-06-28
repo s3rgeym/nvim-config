87262407f6dfd681c8a3b2873bbb5337e960f88e
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2025-06-28 19:25:05 +0300

    переписал сочетания

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,53 +1,58 @@
 -- Тут находятся только базовые сочетания, остальные разбросаны по плагинам и тп
 local map = require("user.utils").keymap
 
+-- Работа с окнами
 map('<leader>q', '<cmd>q<CR>', "Quit window")
 -- Тут такая странная конструкция, чтобы избежать закрытия nvim, если нет
 -- измененных буферов
 map('<leader>x', '<cmd>b#<bar>bd#<CR>', "Close buffer")
 map('<leader>w', '<cmd>write<CR>', "Write file")
 
-map('<C-a>', '<Esc>ggVG', "Select entire buffer", { 'n', 'i' })
+map('<C-a>', 'ggVG', "Select entire buffer")
 
-map('<A-Left>', '<cmd>bp<CR>', "Previous buffer")
-map('<A-Right>', '<cmd>bn<CR>', "Next buffer")
-
-map('<A-Up>', '<cmd>tabprev<CR>', "Previous tab")
-map('<A-Down>', '<cmd>tabnext<CR>', "Next tab")
+-- Переключение между буферами
+map('<Tab>', '<cmd>bn<CR>', "Next buffer")
+map('<S-Tab>', '<cmd>bp<CR>', "Previous buffer")
 
 -- Работа с отступами
-map('<Tab>', '>>_', "Indent")
-map('<S-Tab>', '<<_', "Unindent")
 map('<S-Tab>', '<C-D>', "Unindent", 'i')
 map('<Tab>', '>gv', "Indent selection", 'v')
 map('<S-Tab>', '<gv', "Unindent selection", 'v')
 
-map('<Esc>', '<cmd>nohlsearch<CR><Esc>', "Clear search highlight")
+-- Esc вроде в нормальном режиме и так не работает
+map('<Esc>', '<cmd>nohlsearch<CR>', "Clear search highlight")
 
+-- Стандартное перемещение между окнами
 map('<C-h>', '<C-w>h', "Go to left window")
 map('<C-j>', '<C-w>j', "Go to window below")
 map('<C-k>', '<C-w>k', "Go to window above")
 map('<C-l>', '<C-w>l', "Go to right window")
 
-map('<A-h>', '<cmd>vertical resize -2<CR>', "Decrease width")
-map('<A-l>', '<cmd>vertical resize +2<CR>', "Increase width")
-map('<A-j>', '<cmd>resize -2<CR>', "Decrease height")
-map('<A-k>', '<cmd>resize +2<CR>', "Increase height")
+-- Изменение размеров
+map('<A-Left>', '<cmd>vertical resize -2<CR>', "Decrease width")
+map('<A-Right>', '<cmd>vertical resize +2<CR>', "Increase width")
+map('<A-Down>', '<cmd>resize -2<CR>', "Decrease height")
+map('<A-Up>', '<cmd>resize +2<CR>', "Increase height")
 
+-- Перемещение строк
 map("<A-j>", "<Esc>:m .+1<CR>==gi", "Move line down", 'i')
 map("<A-k>", "<Esc>:m .-2<CR>==gi", "Move line up", 'i')
 map("J", ":m '>+1<CR>gv=gv", "Move selection down", 'v')
 map("K", ":m '<-2<CR>gv=gv", "Move selection up", 'v')
 
+-- Разбиение окон
 map('<leader>sh', '<cmd>split<CR>', "Horizontal split")
 map('<leader>sv', '<cmd>vsplit<CR>', "Vertical split")
 
-map('<leader>ev', '<cmd>split $MYVIMRC<CR>', "Edit Neovim config")
--- lazy.nvim все равно не поддерживает перезагрузку конфига
--- map('<leader>rv', utils.reload_nvim_config, "Reload Neovim config")
+-- Работа с конфигом
+map('<leader>ev', '<cmd>edit $MYVIMRC<CR>', "Edit Neovim config")
+-- lazy.nvim все равно не поддерживает полную перезагрузку плагинов
+map('<leader>rv', '<cmd>source $MYVIMRC<CR>', "Reload Neovim config")
 
+-- Spellcheck. Нужен ли?
 map('<leader>sp', "<cmd>setlocal spell!<cr>", "Toggle spellcheck")
 
+-- Сочетания для терминала
 map("<leader>t", "<cmd>split | terminal<CR>", "Open terminal")
 map("<Esc>", "<C-\\><C-n>", "Escape terminal", "t")
 map("<C-h>", "<C-\\><C-n><C-w>h", "Escape terminal to left window", "t")
--- lua/user/utils.lua
+++ lua/user/utils.lua
@@ -1,8 +1,7 @@
 local M = {}
 
 function M.keymap(lhs, rhs, desc, mode)
-  mode = mode or 'n'
-  vim.keymap.set(mode, lhs, rhs, { desc = desc })
+  vim.keymap.set(mode or 'm', lhs, rhs, { desc = desc })
 end
 
 return M
