8f65873a14f2707c959db74f1d44559896424b64
[object Object] <tz4678@gmail.com>
2025-07-02 00:05:23 +0300

    Update utils.lua

--- lua/user/utils.lua
+++ lua/user/utils.lua
@@ -1,7 +1,25 @@
 local M = {}
 
-function M.map(lhs, rhs, desc, mode)
-  vim.keymap.set(mode or 'n', lhs, rhs, { desc = desc })
+function M.map(mode, lhs, rhs, desc, opts)
+  opts = opts or {}
+  opts.desc = desc
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+function M.nmap(lhs, rhs, desc, opts)
+  M.map('n', lhs, rhs, desc, opts)
+end
+
+function M.imap(lhs, rhs, desc, opts)
+  M.map('i', lhs, rhs, desc, opts)
+end
+
+function M.vmap(lhs, rhs, desc, opts)
+  M.map('v', lhs, rhs, desc, opts)
+end
+
+function M.tmap(lhs, rhs, desc, opts)
+  M.map('t', lhs, rhs, desc, opts)
 end
 
 return M
