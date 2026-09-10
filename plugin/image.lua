630da2307ae97f56d2241ef42f3868def4caa7f4
[object Object] <12753171+s3rgeym@users.noreply.github.com>
2026-09-10 06:21:25 +0300

    Clarify backend support for kitty protocol in image.lua
    Updated comment to clarify terminal support for kitty protocol.

--- plugin/image.lua
+++ plugin/image.lua
@@ -1,5 +1,5 @@
 -- sudo pacman -S imagemagick
 vim.pack.add({ 'https://github.com/3rd/image.nvim' })
 require('image').setup({
-  backend = 'sixel', -- foot не поддерживает kitty
+  backend = 'sixel', -- foot не поддерживает kitty, если же терминал поддерживает kitty протокол, сотри эту строку 
 })
