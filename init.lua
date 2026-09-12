0544deccf4e1b684a48bc4c4bffe6c9f41d8cfe5
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2026-09-12 09:57:51 +0300

    включил новый UI

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 -- Настройки самих плагинов лежат в plugin/ и загружаются стандартным
 -- механизмом Neovim
+-- Пробуем включить новый интерфейс
+pcall(function()
+  require('vim._core.ui2').enable()
+end)
 require('config.options')
 require('config.keymaps')
 require('config.autocmds')
