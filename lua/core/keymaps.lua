a7b404a28a6374cf0b9b421a69b2cbccd47380aa
[object Object] <12753171+s3rgeym@users.noreply.github.com>
2026-09-23 21:58:25 +0300

    Refactor keymap comments and descriptions
    Updated comments for clarity and consistency in key mappings. Adjusted descriptions for various key mappings to enhance understanding.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,31 +1,29 @@
--- Alt + стрелки, f, g, h, j, k, l используются в Zellij, поэтому они не
--- используются в данном конфиге
-
--- Клавиша лидер
+-- Alt + стрелки, f, g, h, j, k, l используются в Zellij, поэтому их избегаем
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- В Neovim в отличии от Vim по умолчанию noremap = true, поэтому
--- пользовательские сочетания в rhs не будут разворачиваться,
--- будут работать только встроенные
+-- В Neovim по умолчанию noremap = true, т.е. только встроенные сочетания будут работать в rhs
+-- Это позволяет избежать рекурсии
+-- silent = true подавляет вывод команд в строку сообщений.
+-- Он нужен для vim-команд (строки, нач-ся с `:`, `<Cmd>`)
+-- Не нужен для vim.cmd.* и там где есть интерактив и важен вывод.
 local map = vim.keymap.set
 
+-- Общие сочетания
 map('n', '<leader>q', vim.cmd.quit, { desc = 'Quit' })
 map('n', '<leader>w', vim.cmd.write, { desc = 'Save' })
--- Я это сочетание редко использую
--- map('n', '<leader>a', 'ggVG', { desc = 'Select [a]ll' })
+-- Для выделения и копирования - :%y
+-- map('n', '<leader>a', 'ggVG', { desc = 'Select all' })
 -- <C-w>c
 -- map('n', '<leader>bc', vim.cmd.close, { desc = 'Close buffer' })
--- Esc ничего не делает в нормальном режиме в Neovim. Я не помню почему в Vim
--- на него нельзя было повесить очистку экрана
-map('n', '<Esc>', '<cmd>noh<cr><esc>', { desc = 'Clear search highlight' })
+-- В Neovim никаких сочетаний нет для Escape в нормальном режиме
+map('n', '<Esc>', '<cmd>noh<cr>', { desc = 'Clear search highlight' })
 
--- Buffers
--- H и L по умолчанию служат для перехода в начало и конец буфера
-map('n', 'H', vim.cmd.bprev, { desc = 'Previous Buffer' })
-map('n', 'L', vim.cmd.bnext, { desc = 'Next Buffer' })
--- Эти сочетания нужны очень редко, я бы задумался об их необходимости
--- <leader>d и <leader>x под плагины оставил
+-- Буферы
+-- H/L переходят по буферам вместо верха/низа экрана
+map('n', 'H', vim.cmd.bprev, { desc = 'Previous buffer' })
+map('n', 'L', vim.cmd.bnext, { desc = 'Next buffer' })
+-- Эти сочетания редко используются
 map(
   'n',
   '<leader>bd',
@@ -39,85 +37,91 @@ map(
   { desc = 'Delete other buffers' }
 )
 
--- windows
-map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window' })
-map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window' })
-map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window' })
-map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window' })
+-- Окна
+map('n', '<C-h>', '<C-w>h', { desc = 'Go to left window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Go to lower window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Go to upper window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Go to right window' })
 
-map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase height' })
-map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease height' })
-map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease width' })
+map('n', '<C-Up>', '<cmd>resize +2<cr>', {
+  desc = 'Increase window height',
+})
+map('n', '<C-Down>', '<cmd>resize -2<cr>', {
+  desc = 'Decrease window height',
+})
+map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', {
+  desc = 'Decrease window width',
+})
 map(
   'n',
   '<C-Right>',
   '<cmd>vertical resize +2<cr>',
-  { desc = 'Increase width' }
+  { desc = 'Increase window width' }
 )
 
 map('n', '<leader>h', vim.cmd.split, { desc = 'Horizontal split' })
 map('n', '<leader>v', vim.cmd.vsplit, { desc = 'Vertical split' })
 
--- Tabs
--- Выбор таба с помощью Alt+1..9
+-- Вкладки
 for i = 1, 9 do
-  map('n', '<a-' .. i .. '>', i .. 'gt', { desc = 'Go to Tab ' .. i })
+  map('n', '<a-' .. i .. '>', i .. 'gt', { desc = 'Go to tab ' .. i })
 end
 
 map('n', '<A-0>', vim.cmd.tablast, { desc = 'Go to last tab' })
 map('n', '<leader>tn', vim.cmd.tabnew, { desc = 'New tab' })
 map('n', '<leader>tc', vim.cmd.tabclose, { desc = 'Close tab' })
 
--- движение по переносам строк
--- Вместо v лучше всегда использовать x, если не предполагается работа в режиме Select.
--- v включает режим визуального выделения (Visual) и режим замены выделения (Select).
--- x работает только в визуальном режиме (Visual), что предотвращает случайный перехват
--- клавиш в режиме Select.
+-- Навигация
+-- Нужно всегда x использовать вместо v
+-- x - Visual, т.е. визуальный режим, только выделение
+-- v - Visual + Select. В Select выделение заменяется на вводимые символы
 map(
   { 'n', 'x' },
   'j',
   "v:count == 0 ? 'gj' : 'j'",
-  { expr = true, silent = true }
+  { expr = true }
 )
 map(
   { 'n', 'x' },
   '<Down>',
   "v:count == 0 ? 'gj' : 'j'",
-  { expr = true, silent = true }
+  { expr = true }
 )
 map(
   { 'n', 'x' },
   'k',
   "v:count == 0 ? 'gk' : 'k'",
-  { expr = true, silent = true }
+  { expr = true }
 )
 map(
   { 'n', 'x' },
   '<Up>',
   "v:count == 0 ? 'gk' : 'k'",
-  { expr = true, silent = true }
+  { expr = true }
 )
 
--- Перемещение строк
-map('x', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move Selection Up' })
-map('x', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move Selection Down' })
+-- Перемещение выделения
+map('x', 'K', ":m '<-2<CR>gv=gv", {
+  desc = 'Move selection up',
+  silent = true,
+})
+map('x', 'J', ":m '>+1<CR>gv=gv", {
+  desc = 'Move selection down',
+  silent = true,
+})
 
 -- Отступы
--- Не будет работать, например, если в буфере в режиме вставки повесить на него действие
+-- Это сочетание не будет работать, так как на Shift-Tab вешают выбор предыдущего элемента из списка
 -- map('i', '<S-Tab>', '<C-d>', { desc = 'Outdent line' })
-map('v', '<Tab>', '>gv', { desc = 'Indent' })
-map('v', '<S-Tab>', '<gv', { desc = 'Outdent' })
+map('v', '<Tab>', '>gv', { desc = 'Indent selection' })
+map('v', '<S-Tab>', '<gv', { desc = 'Outdent selection' })
 map('n', '<Tab>', '>>', { desc = 'Indent line' })
 map('n', '<S-Tab>', '<<', { desc = 'Outdent line' })
 
--- Просто Enter для отображения справки
 -- map('n', '<cr>', '<C-]>', { desc = 'Help' })
 
--- Замена строк от s — substitute
-
--- Для замены во всех файлах с расширением питон:
--- :args **/*.py
--- :argdo %s/\<старое_слово\>/новое_слово/g | update
+-- Поиск и замена
+-- Замена во всех файлах: :args **/*.py | :argdo %s/\<old\>/new/g | update
 map(
   'n',
   '<leader>s',
@@ -131,15 +135,14 @@ map(
   { desc = 'Replace selection' }
 )
 
--- Neovim
+-- Конфиги Neovim
 map(
   'n',
   '<leader>ev',
   '<cmd>edit $MYVIMRC<cr>',
-  { desc = 'Edit Neo[v]im Config' }
+  { desc = 'Edit Neovim config' }
 )
--- <leader>r оставил для других сочетаний
-map('n', '<leader>rv', vim.cmd.restart, { desc = 'Restart Neo[v]im' })
+map('n', '<leader>rv', vim.cmd.restart, { desc = 'Restart Neovim' })
 
 -- Управление плагинами
 map('n', '<leader>pu', vim.pack.update, { desc = 'Update plugins' })
@@ -154,4 +157,4 @@ map('n', '<leader>pc', function()
       return plugin.spec.name
     end)
     :totable())
-end, { desc = 'Clean plugins' })
+end, { desc = 'Clean inactive plugins' })
