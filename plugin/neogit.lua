39c7ebfb38667b6de70d9695a741ba3498341978
[object Object] <12753171+s3rgeym@users.noreply.github.com>
2026-09-23 22:14:31 +0300

    Add commit instructions to neogit.lua
    Added standard commit instructions for Neogit usage.

--- plugin/neogit.lua
+++ plugin/neogit.lua
@@ -1,5 +1,6 @@
 -- Хелпер для работы с Git, нетребующий стронних утилит
-
+-- Стандартный коммит: <C-s>, c, a, ввод сообщения, <Esc>, :w, <C-c>, P, p
+-- Команду проще ввести, `:!git commit -am '...'; git push`, но тут как-то нагляднее
 vim.pack.add({
   'https://github.com/sindrets/diffview.nvim',
   'https://github.com/NeogitOrg/neogit',
